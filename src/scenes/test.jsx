eec488290134672a8a936387feb042e04eb553af
shaktimaan29 <riteshlavti@gmail.com>
2023-09-21 13:45:33 +0530

    first commit

---
+++ src/scenes/test.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world! Our project is under construction kindly visit later
+        
+      </h1>
+    )
+  }
